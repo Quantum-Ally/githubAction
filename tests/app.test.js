a7caad850285e225c452ac5f473f0fa3a724c908
Quantum-Ally <abdulqudoos7113@gmail.com>
2025-03-12 13:45:46 +0500

    Fix test configuration and add proper server cleanup

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,14 @@
 const request = require('supertest');
 const app = require('../src/app');
+let server;
+
+beforeAll(() => {
+  server = app.listen(0); // Use any available port
+});
+
+afterAll((done) => {
+  server.close(done);
+});
 
 describe('API Tests', () => {
   test('GET / should return Hello, World!', async () => {
